dd4be6c27eb542c2c40e9755951d5a615b8ceedf
Ayman Elkassas <aymanelkassas88@gmail.com>
2020-04-23 04:37:47 +0200

    Complex Elastic Search Query

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,29 @@ use Illuminate\Support\Facades\Route;
 use \App\Article;
 
 Route::get('/', function () {
+
+    //First Step should add index to elastic search engine
     Article::addAllToIndex();
 
     return view('welcome');
 });
 
 Route::get('search',function (){
+
     $articles = Article::searchByQuery(
         [
-            'match' => ['body' => 'Beatae'],
-            'match'=>['tags'=>'maxime']
-        ]);
+            'match' => ['title' => 'sed'],
+            //match on second filter and override one
+            'match' =>['tags'=>'maxime']
+        ],null,null,3,null,null);
+
+    //return number of object return
+//    dd($articles->totalHits());
+//    dd( $articles->count());
+//    dd($articles->skip(2)->get(2))
+
+    //Put first two object in array
+//    dd($articles->chunk(2));
 
     return $articles;
 });
