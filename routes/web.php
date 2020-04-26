0dd591d8b1399b6d766e4e0eb8d39e4c0ecc5793
Ayman Elkassas <aymanelkassas88@gmail.com>
2020-04-26 20:58:30 +0200

    Template Full Elastic Search

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,70 @@ Route::get('/', function () {
 
 Route::get('search',function (){
 
+    //todo:Simple and Quick Search Query
+//    $articles = Article::searchByQuery(
+//        [
+//            'match' => ['title' => 'sed'],
+//            //match on second filter and override one
+////            'match' =>['tags'=>'maxime']
+//        ],null,null,3,null,null
+//    );
+
+    //todo:Full search example with aggregation, highlight, filter...
     $articles = Article::searchByQuery(
         [
-            'match' => ['title' => 'sed'],
-            //match on second filter and override one
-            'match' =>['tags'=>'maxime']
-        ],null,null,3,null,null);
+            //QUERY
+            'bool' => [
+                'must'=>[
+                    'match'=> [
+                        //match : title must entire word like : 'sed'
+                        'title'=> 'sed'
+                    ],
+                ],
+                //then result apply next query
+                'must_not'=> [
+                    [
+                        //match_phrase : title must not entire phrase like : 'Quidem quis sed.'
+                        'match_phrase' =>[
+                            'title' => 'Quidem quis sed.'
+                        ]
+                    ]
+                ],
+                'filter'=>[
+                    [
+                        'exists'=>[
+                            'field'=>'title'
+                        ],
+                    ],
+                    [
+                        'range'=>[
+                            'id'=>[
+                                'gte'=>10
+                            ]
+                        ]
+                    ]
+                ],
+                'should'=>[
+                    'match'=> [
+                        //match : title must entire word like : 'sed'
+                        'title'=> 'sed'
+                    ]
+                ],
+                'minimum_should_match' => 1,
+            ],
+        ],
+        [
+            //AGGREGATION
+//            'aggs'=> [
+//                'my_agg'=> [
+//                    'terms' => [
+//                        'field' => 'title',
+//                        'size' => 10
+//                        ]
+//                    ]
+//            ]
+        ],null,3,null, ['id'=> ['order'=>'asc']]
+    );
 
     //return number of object return
 //    dd($articles->totalHits());
